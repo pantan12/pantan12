ccb3eb5f0afacea5c6447ed341524ba09cbf46a6
pantan12 <jms2010922@gmail.com>
2024-11-02 12:01:25 +0900

    s

---
+++ board/public/js/view.js
@@ -0,0 +1,33 @@
+const boardsStr = localStorage.getItem("boards");
+const boardsObj = JSON.parse(boardsStr);
+
+const idx = location.search;
+const index = idx.split("=")[1];
+const board = boardsObj[index];
+
+const beforeUrl = document.referrer;
+
+const viewCount = (beforeUrl) => {
+  if (beforeUrl.split("/").pop() === "list.html") {
+    board.views++;
+    const viewCountStr = JSON.stringify(boardsObj);
+    localStorage.setItem("boards", viewCountStr);
+  }
+};
+
+viewCount(beforeUrl);
+
+const viewFrm = document.querySelectorAll("#viewFrm > div");
+
+for (let i = 0; i < viewFrm.length; i++) {
+  const id = viewFrm[i].id;
+  viewFrm[i].innerHTML += " " + board[id];
+}
+
+const modifyBtn = document.querySelector("#modify");
+
+const modifyBtnHandler = (e) => {
+  location = "C:/Users/SAMSUNG/Documents/Visual Studio 2022/pantan12/board/public/js/modify.js" + idx;
+};
+
+modifyBtn.addEventListener("click", modifyBtnHandler);
